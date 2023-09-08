a5c9d316700eeca6d9e9dcd7639e2e261d9c7740
Bashua Mutiat <mutiatbashua@gmail.com>
2023-09-08 10:14:12 +0100

    updated files

--- script.js
+++ script.js
@@ -7,13 +7,17 @@ const daysOfWeek = [
   "Friday",
 ];
 const currentDate = new Date();
+console.log(currentDate);
 const currentDay = daysOfWeek[currentDate.getDay()];
-// const currentTime = currentDate.toUTCString();
-const currentTime = currentDate.getTime();
+const hours = currentDate.getUTCHours();
+const minutes = currentDate.getUTCMinutes();
+const milliseconds = currentDate.getUTCMilliseconds();
+const currentTime = `${hours} : ${minutes} : ${milliseconds}`;
+console.log(currentTime);
 
 //  to display the current day and time
-const day = document.querySelector('[data-testid="currentDayOfTheWeek"]');
-const time = document.querySelector('[data-testid="curretUTCTime"]');
+const day = document.querySelector("#day");
+const time = document.querySelector("#time");
 
-day.textContent = { currentDay };
-time.textContent = { currentTime };
+day.textContent = `${currentDay}`;
+time.textContent = `${currentTime}`;
